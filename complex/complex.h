b6cdd3208c5b8d6a4a837262c1ba188e2952f860
dzyubaira <32271744+dzyubaira@users.noreply.github.com>
2017-11-14 22:52:27 +0300

    Update complex.h

--- complex/complex.h
+++ complex/complex.h
@@ -1,14 +1,13 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 #include <iosfwd>
-#include <iostream>
 
 struct Complex
 {
 	Complex() {}
 	explicit Complex(const double real);
 	Complex(const double real, const double imaginary);
-	bool operator==(const Complex& rhs) const { return (re == rhs.re) && (im == rhs.im); }
+	bool operator==(const Complex& rhs) const { return (abs(re - rhs.re)<pow(10,-7)) && (abs(im - rhs.im)<pow(10,-7)); }
 	bool operator!=(const Complex& rhs) const { return !operator==(rhs); }
 	Complex& operator+=(const Complex& rhs);
 	Complex& operator+=(const double rhs) { return operator+=(Complex(rhs)); }
