9ec79d2cef8245739e51f818066e3286dd7dd89c
dzyubaira <32271744+dzyubaira@users.noreply.github.com>
2017-11-15 18:27:27 +0300

    Update array_test.cpp

--- array/array_test.cpp
+++ array/array_test.cpp
@@ -5,10 +5,12 @@
 int main()
 {
 	using namespace std;
-	Array a(3);
+	Array a(5);
 	a[0] = 4;
 	a[1] = 4;
 	a[2] = 6;
+	a[3] = 7;
+	a[4] = 8;
 	cout << "Array a:  " << a << endl;
 	Array b(a);
 	cout << "Array b(a):  " << b << endl;
@@ -17,14 +19,11 @@ int main()
 	cout << "Array c = b:  " << c << endl;
 	int d = c.length();
 	cout << "length(c) = " << d << endl;
-	c.insert(3);
-	cout << "c.insert(3) = " << c << endl;
-	int e = c.length();
-	cout << "length(c) = " << e << endl;
 	c.remove(4);
-	cout << "c.remove(4) = " << c << endl;
-	int f = c.length();
-	cout << "length(c) = " << f << endl;
-	system("pause");
+	cout << "c.remove(4) = " << c << endl << "length(c) = " << c.length() << endl;
+	c.insert_i(1, 5);
+	cout << "c.insert_i(1,5) = " << c << endl << "length(c) = " << c.length() << endl;
+	c.remove_i(1);
+	cout << "c.remove_i(1) = " << c << endl << "length(c) = " << c.length() << endl;
 	return 0;
 }
